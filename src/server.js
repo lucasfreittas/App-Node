f21eba6a3a13a77ec6832c270072338297e7f480
lucasfreittas <l.freittas@outlook.com>
2022-05-25 19:07:33 -0300

    POST Method with Insomnia

--- src/server.js
+++ src/server.js
@@ -3,16 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 3333;
 
-app.get('/message/:id/:name', (request, response) => {
+app.post('/users', (request, response) => {
 
-    const { id, user} = request.params;
-
-    response.send(`Usuário: ${id} e ID: ${user}`);
-});
-
-app.get('/users', (request, response) => {
-    const {page, limit} = request.query;
-    response.send(`Página: ${page}, Limite: ${limit}`)
+    response.send(`Você chamou o POST`)
 })
 
 app.listen(PORT, () => {console.log(`Express is runing on PORT ${PORT}`)});
