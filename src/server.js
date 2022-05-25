624112f00eb51b721887b1b24853db1f88dca041
lucasfreittas <l.freittas@outlook.com>
2022-05-25 18:38:21 -0300

    Request for params

--- src/server.js
+++ src/server.js
@@ -3,4 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = 3333;
 
-app.listen(PORT, () => {console.log(`Express is runing on PORT ${PORT}`)})
+app.get('/message/:id/:name', (request, response) => {
+
+    const { id, user} = request.params;
+
+    response.send(`Usuário: ${id} e ID: ${user}`);
+});
+
+app.listen(PORT, () => {console.log(`Express is runing on PORT ${PORT}`)});
