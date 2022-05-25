8972a0cc5c51ad1f1010d351e2d491c622340e56
lucasfreittas <l.freittas@outlook.com>
2022-05-25 19:14:01 -0300

    How to capture info from post json

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 const PORT = 3333;
+app.use(express.json())
 
 app.post('/users', (request, response) => {
 
-    response.send(`Você chamou o POST`)
+    const { name, email, password } = request.body;
+    response.json({ name, email, password})
 })
 
 app.listen(PORT, () => {console.log(`Express is runing on PORT ${PORT}`)});
