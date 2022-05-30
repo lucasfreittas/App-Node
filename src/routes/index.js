88bae6f73db0c51b829c0a81cf0ab2f3cb498b34
lucasfreittas <l.freittas@outlook.com>
2022-05-30 17:52:29 -0300

    bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,9 @@ const usersRouter = require('./users.routes'); // Estamos requisitando a página
 const notesRouter = require('./notes.routes');
 
 const routes = Router(); // Estamos inicializando o Router do express (requisitado na linha 1) e colocando dentro de uma constante chamada routes que irá conter todas as rotas da nossa aplicação
-const notes = Router()
+
 
 routes.use("/users", usersRouter); // Ao passar a pasta routes "no server.js" e pessoa requisitar "/users" irá cair nessa linha e será mandado para a pagina "users.routes" que passamos para dentro dessa constante usersRouter na linha 3
-notes.use("/notes", notesRouter);
+routes.use("/notes", notesRouter);
 
 module.exports = routes; // Estamos exportando todo o Router() para toda a aplicação
