a0b19f5e90ea73c81befecc4cde41baecab137dd
lucasfreittas <l.freittas@outlook.com>
2022-06-03 15:14:05 -0300

    cascade fixed

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,13 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    
+    pool: {
+      afterCreate: function (conn, cb) {
+        conn.run('PRAGMA foreign_keys = ON', cb)
+      }
+    },
+
     migrations:{
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations" )
     },
